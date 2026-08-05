dbde0a1fe15ca023a36e63c880ed089ed6e51a09
paulacarolina203040-max <paulacarolina203040@gmail.com>
2026-08-05 20:16:29 -0300

    Fixed again distance class

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from typing import Self
-
 
 class Distance:
     def __init__(self, km: float) -> None:
@@ -27,10 +25,7 @@ class Distance:
     ) -> Distance:
         return self.__add__(other)
 
-    def __iadd__(
-        self,
-        other: Distance | float
-    ) -> Self:
+    def __iadd__(self, other: Distance | float):
         if isinstance(other, Distance):
             self.km += other.km
         else:
