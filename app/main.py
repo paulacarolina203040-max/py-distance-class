e5aa9f77b717ed174834ace813e3a6751bf103b4
paulacarolina203040-max <paulacarolina203040@gmail.com>
2026-08-05 21:50:36 -0300

    Fixed one time more

--- app/main.py
+++ app/main.py
@@ -32,13 +32,11 @@ class Distance:
         self.km = result.km
         return self
 
-    def __mul__(
-        self,
-        other: float
-    ) -> Distance:
+    def __mul__(self, other: float) -> Distance:
         if isinstance(other, Distance):
-            raise TypeError("Distance cannot be multiplied by another Distance"
-            )
+            raise TypeError(
+                "Distance cannot be multiplied by another Distance"
+                            )
         return Distance(self.km * other)
 
     def __rmul__(
